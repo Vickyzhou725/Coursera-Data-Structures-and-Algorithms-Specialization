434e1dec433e54a00e8c46d8c5f11fcb977ca9d2
Vickyzhou725 <qz256@cornell.edu>
2022-02-06 00:31:11 -0500

    creating auto tests

--- Course 2 - Data Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ Course 2 - Data Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -18,6 +18,22 @@ class StackWithMax():
         return max(self.__stack)
 
 
+class QueueWithMax():
+    def __init__(self):
+        self.__stack = []
+
+    def Push(self, a):
+        self.__stack.append(a)
+
+    def Pop(self):
+        assert (len(self.__stack))
+        self.__stack.pop()
+
+    def Max(self):
+        assert (len(self.__stack))
+        return max(self.__stack)
+
+
 if __name__ == '__main__':
     stack = StackWithMax()
 
--- Course 2 - Data Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
+++ Course 2 - Data Structures/week4_binary_search_trees/1_tree_traversals/tree-orders.py
@@ -8,16 +8,13 @@ threading.stack_size(2 ** 27)  # new thread will get stack of such size
 
 
 class TreeOrders:
-    def read(self, file_name):
-        file = open(file_name, 'r')
-        lines = file.read().splitlines()
-        # self.n = int(sys.stdin.readline())
-        self.n = int(lines[0])
+    def read(self, size, tree_nodes):
+        self.n = int(size)     # self.n = int(sys.stdin.readline())
         self.key = [0 for i in range(self.n)]
         self.left = [0 for i in range(self.n)]
         self.right = [0 for i in range(self.n)]
         for i in range(self.n):
-            [a, b, c] = map(int, lines[i+1].split())  # sys.stdin.readline().split())
+            [a, b, c] = map(int, tree_nodes[i].split())  # sys.stdin.readline().split())
             self.key[i] = a
             self.left[i] = b
             self.right[i] = c
@@ -74,14 +71,52 @@ class TreeOrders:
                 q.put(self.right[root])
         return self.result
 
+    def test(self, outputs):
+        in_order = " ".join([str(x) for x in self.inOrder(0)])
+        pre_order = " ".join([str(x) for x in self.preOrder(0)])
+        post_order = " ".join([str(x) for x in self.postOrder(0)])
+        level_order = " ".join([str(x) for x in self.levelOrder(0)])
+        try:
+            assert outputs[0] == in_order
+            print('in_order test success!')
+        except:
+            print('in_order test failed!')
+            print(f'anticipated: {outputs[0]}')
+            print(f'returned: {in_order}')
+        try:
+            assert outputs[1] == pre_order
+            print('pre_order test success!')
+        except:
+            print('pre_order test failed!')
+            print(f'anticipated: {outputs[1]}')
+            print(f'returned: {pre_order}')
+        try:
+            assert outputs[2] == post_order
+            print('post_order test success!')
+        except:
+            print('post_order test failed!')
+            print(f'anticipated: {outputs[2]}')
+            print(f'returned: {post_order}')
+        try:
+            assert outputs[3] == level_order
+            print('level_order test success!')
+        except:
+            print('level_order test failed!')
+            print(f'anticipated: {outputs[3]}')
+            print(f'returned: {level_order}')
+
 
 def main():
-    tree = TreeOrders()
-    tree.read(file_name=r'week4_binary_search_trees/1_tree_traversals/tests/test2')
-    print(" ".join(str(x) for x in tree.inOrder(0)))
-    print(" ".join(str(x) for x in tree.preOrder(0)))
-    print(" ".join(str(x) for x in tree.postOrder(0)))
-    print(" ".join(str(x) for x in tree.levelOrder(0)))
+    for i in range(2):
+        print(f'test case {i}')
+        file_name = r'C:\Users\qz256\PycharmProjects\Coursera-Data-Structures-and-Algorithms-Specialization' \
+                    r'\Course 2 - Data Structures\week4_binary_search_trees\1_tree_traversals\tests\test' + str(i)
+        file = open(file_name, 'r')
+        lines = file.read().splitlines()
+        size, tree_nodes, outputs = lines[0], lines[1:-4], lines[-4:]
+        tree = TreeOrders()
+        tree.read(size, tree_nodes)
+        tree.test(outputs)
 
 
 threading.Thread(target=main).start()
